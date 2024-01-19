56445d1a15cc6a608fd4fddc8a21e68bcf318c19
Harshit-3905 <harshitjoshileet@gmail.com>
2024-01-19 12:57:40 +0530

    Add CORS support and update dependencies

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import app from "./app.js";
 
 dotenv.config();
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        });
+    })
+    .catch((err) => console.log("Database Connection Failed"));
