8de8a4bf9744fae102beb6d54b30eb458ce747b0
Harshit-3905 <harshitjoshileet@gmail.com>
2024-01-29 16:14:06 +0530

    Add deleteFromCloudinary function to handle deleting images from Cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -19,4 +19,21 @@ const uploadOnCloudinary = async (file) => {
     }
 };
 
-export { uploadOnCloudinary };
+const deleteFromCloudinary = async (url) => {
+    try {
+        cloudinary.config({
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET,
+        });
+        const parts = url.split("/");
+        const idWithExtension = parts[parts.length - 1];
+        const public_id = idWithExtension.split(".")[0];
+        const res = await cloudinary.uploader.destroy(public_id);
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export { uploadOnCloudinary, deleteFromCloudinary };
