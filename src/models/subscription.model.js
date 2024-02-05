4f36ee305891c61cd81bed0d25fbfed5dfd16ba3
Harshit-3905 <harshitjoshileet@gmail.com>
2024-02-05 17:19:09 +0530

    Add required field for subscriber in subscription model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -5,6 +5,7 @@ const subscriptionSchema = new mongoose.Schema(
         subscriber: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
+            required: true,
         },
         channel: {
             type: mongoose.Schema.Types.ObjectId,
