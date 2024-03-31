677276a05b717c0e1d37adecfac5109e494bfa92
Harshit-3905 <harshitjoshileet@gmail.com>
2024-03-31 17:09:05 +0530

    Update .env.sample and redis.js

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -2,10 +2,7 @@ import { Redis } from "ioredis";
 
 let redisClient = null;
 export default function connectRedis() {
-    const client = new Redis({
-        host: "redis",
-        port: 6379,
-    });
+    const client = new Redis(process.env.REDIS_URL);
     client.on("connect", () => {
         console.log("Redis connected");
     });
